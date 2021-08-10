707a3b124b4981c60b6d787772c19527291697c8
Nikita <ussnikita209@mail.ru>
2021-08-10 15:05:09 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if (array != null && array.length != 0) {
+    let min = Infinity
+    for (el of array) {
+      min = el < min ? el : min
+    }
+    return min
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array != null && array.length != 0) {
+    let max = -Infinity
+    for (el of array) {
+      max = el > max ? el : max
+    }
+    return max
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array != null && array.length != 0) {
+    const sum = array.reduce((sum, val) => sum += val, 0)
+    return sum / array.length
+  }
   return 0;
 }
